8c6bcae6b2a78ca43c87fdde47cfc2163b1c0900
Brutus Buckeye <buckeye@publics.local>
2017-01-20 17:46:28 -0500

    finishied 26 easy

---
+++ 26_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length < 2) return nums.length;
+        int j = 0;
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] != nums[j]){
+                nums[j + 1] = nums[i];
+            }
+        }
+
+        return j + 1;
+    }
+}
